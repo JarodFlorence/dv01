17172bf1a50682607fc09cb321147fb1afe91559
JarodFlorence <jarod.a.florence@gmail.com>
2023-09-19 13:20:29 +0330

    Convert the grade object to an array

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -6,16 +6,16 @@ const DataTable = () => {
 
   return (
     <div className="flex border border-gray-300">
-      {Object.keys(grades).map((grade) => (
+      {grades.map(({ grade, total }) => (
         <div
           key={grade}
           className="flex flex-col flex-1 border-r border-gray-300 min-w-[10rem] last:border-r-0"
         >
-          <span className="text-center py-4 border-b border-gray-300 bg-gray-100 font-bold text-sm">
+          <span className="text-center text-primary py-4 border-b border-gray-300 bg-gray-100 font-bold text-sm">
             Grade {grade}
           </span>
           <span className="text-center py-5">
-            ${numberWithCommas(Number(grades[grade]).toFixed(2))}
+            ${numberWithCommas(Number(total).toFixed(2))}
           </span>
         </div>
       ))}
--- src/hooks/useDataStore.js
+++ src/hooks/useDataStore.js
@@ -4,13 +4,18 @@ const useDataStore = create((set, get) => ({
   data: [],
   grades: () => {
     // create an object based on the grade number with a total aggregation of the current balance for each grade
-    return get().data.reduce((grades, item) => {
+    const gradesObj = get().data.reduce((grades, item) => {
       if (!item.grade) return grades;
       return {
         ...grades,
         [item.grade]: (grades[item.grade] || 0) + Number(item.currentBalance),
       };
     }, {});
+    // convert the grade object to an array
+    return Object.keys(gradesObj).map((grade) => ({
+      grade,
+      total: gradesObj[grade],
+    }));
   },
   setData: (data) => set({ data }),
 }));
