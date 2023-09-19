06c59422f31747ba20c343b9da7b5aa251a29a5c
JarodFlorence <jarod.a.florence@gmail.com>
2023-09-19 11:48:29 +0330

    Move the loansize.csv file to the 'public'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
+import { getData } from "./request/api";
+
 function App() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await getData();
+    console.log(res);
+  };
+
   return (
     <div className="flex flex-1 justify-center items-center">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
