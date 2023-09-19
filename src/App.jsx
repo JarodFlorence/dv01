a2c1c304fd7ca35b3f791cc210b0750358bda5e0
JarodFlorence <jarod.a.florence@gmail.com>
2023-09-19 11:36:29 +0330

    Initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
